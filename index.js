cbc415879183b2f9a4f26a545ea370aba19a4ec3
Lemachipascal-dev <lemachijo421@gmail.com>
2024-07-18 10:06:08 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+
+function saturdayFun(activity= 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`
+}
+saturdayFun();
+saturdayFun('bathe my dog')
+
+const mondayWork = function (ToDo = 'go to the office'){
+    return `This Monday, I will ${ToDo}.`
+}
+mondayWork()
+mondayWork('work from home.')
+
+function wrapAdjective (symbol = '*'){
+    return function (adjective='special'){
+        return `You are ${symbol}${adjective}${symbol}!`
+    }
+
+}
+const wrapStars = wrapAdjective()
+const wrapBars = wrapAdjective('||')
